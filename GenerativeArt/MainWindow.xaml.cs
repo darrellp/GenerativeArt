ed32a00100ec5e6e12e7974a7db197983fce7b98
Darrell Plank <jar_czar@msn.com>
2023-04-13 16:41:06 -0700

    Add project files.

---
+++ GenerativeArt/MainWindow.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using MathNet.Numerics.Distributions;
+
+namespace GenerativeArt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        private const double NoiseScale = 1000.0;
+        private const double StdDev = 0.15;
+        private const double Mean = 0.5;
+        private int cPoints = 6000000;
+        private readonly Normal _distNormal;
+        private int width, height;
+
+        private WriteableBitmap? wbmp;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _distNormal = new(Mean, StdDev);
+        }
+
+        private void OnGenerate(object sender, RoutedEventArgs e)
+        {
+            wbmp.Clear(Colors.Black);
+            var noise = new Perlin() { Frequency = 2.5, Persistence = 5, Octaves = 2 };
+
+            //for (int iRow = 0; iRow < height; iRow++)
+            //{
+            //    for (int iCol = 0; iCol < width; iCol++)
+            //    {
+            //        var noiseVal = noise.Value(iRow / (double)height, iCol / (double)width);
+            //        var colorByte = (byte)Math.Min(255, noiseVal * 255);
+            //        var color = Color.FromRgb(colorByte, colorByte, colorByte);
+            //        wbmp.SetPixel(iCol, iRow, color);
+            //    }
+            //}
+            var maxHits = 0;
+            var hits = new int[width, height];
+            var R = new int[width, height];
+            var G = new int[width, height];
+            var B = new int[width, height];
+
+            for (var ipt = 0; ipt < cPoints; ipt++)
+            {
+                var (pt, clr) = CalcNebulaPoint(noise);
+                var xPix = (int)(pt.X + 0.5);
+                var yPix = (int)(pt.Y + 0.5);
+
+                if (xPix >= 0 && xPix < width && yPix >= 0 && yPix < height)
+                {
+                    R[xPix, yPix] += clr.R;
+                    G[xPix, yPix] += clr.G;
+                    B[xPix, yPix] += clr.B;
+
+                    var hitsCur = ++hits[xPix, yPix];
+                    if (hitsCur > maxHits)
+                    {
+                        maxHits = hitsCur;
+                    }
+                }
+            }
+
+            double maxHitsDbl = maxHits;
+
+            for (var iX = 0; iX < width; iX++)
+            {
+                for (var iY = 0; iY < height; iY++)
+                {
+                    var hitCount = hits[iX, iY];
+                    if (hitCount == 0)
+                    {
+                        continue;
+                    }
+                    var noiseVal = Math.Pow(hitCount / maxHitsDbl, 1.0 / 5.0);
+                    if (noiseVal == 0.0)
+                    {
+                        continue;
+                    }
+                    //var colorByte = (byte)Math.Min(255, noiseVal * 255);
+                    //var color = Color.FromRgb(colorByte, colorByte, colorByte);
+                    var r = (byte)(R[iX, iY] * noiseVal / hitCount);
+                    var g = (byte)(G[iX, iY] * noiseVal / hitCount);
+                    var b = (byte)(B[iX, iY] * noiseVal / hitCount);
+                    var color = Color.FromRgb(r, g, b);
+                    wbmp.SetPixel(iX, iY, color);
+                }
+            }
+        }
+        private void Rectangle_Loaded(object sender, RoutedEventArgs e)
+        {
+            width = (int)RctSize.ActualWidth;
+            height = (int)RctSize.ActualHeight;
+            wbmp = BitmapFactory.New(width, height);
+            Art.Source = wbmp;
+            OnGenerate(this, new RoutedEventArgs());
+        }
+
+        private readonly Color _clrInner = Colors.Yellow;
+        private readonly Color _clrOuter = Colors.Red;
+        private const double SqrtTwo = 1.41421356237;
+
+        private (Point, Color) CalcNebulaPoint(Perlin noise)
+        {
+            var xNorm = (float)_distNormal.Sample(); // take random point from gaussian distribution
+            var yNorm = (float)_distNormal.Sample();
+            var x = xNorm * width; // take random point from gaussian distribution
+            var y = yNorm * height;
+            var xc = xNorm - 0.5;
+            var yc = yNorm - 0.5;
+            var dist = Math.Pow(xc * xc + yc * yc, 1/6.0);
+            var tColor = dist / SqrtTwo;
+            var r = NoiseScale * Math.Sqrt(xNorm * xNorm + yNorm * yNorm); // calculate noise scaling factor from distance
+
+            // Pick two uncorrelated points using the Z axis
+            var nx = r * (noise.Value(xNorm, yNorm, 0.75) - 0.5);
+            var ny = r * (noise.Value(xNorm, yNorm, 0.25) - 0.5);
+
+            return (new Point(x + nx, y + ny), NebulaColor(5, tColor));
+            // return (new Point(x + nx, y + ny), LerpColor(_clrInner, _clrOuter, tColor));
+        }
+
+        private Color NebulaColor(int cBands, double t, bool fHardEdge = false)
+        {
+            var band = cBands * t;
+            var iBand = (int)Math.Floor(band);
+            if (fHardEdge)
+            {
+                return (iBand & 1) == 0 ? _clrInner : _clrOuter;
+            }
+            var tBand = band - iBand;
+            var color1 = _clrInner;
+            var color2 = _clrOuter;
+
+            if ((iBand & 1) == 0)
+            {
+                (color1, color2) = (color2, color1);
+            }
+
+            return LerpColor(color1, color2, tBand);
+        }
+
+        private static Color LerpColor(Color color1, Color color2, double t)
+        {
+            var r = color1.R * (1 - t) + color2.R * t;
+            var g = color1.G * (1 - t) + color2.G * t;
+            var b = color1.B * (1 - t) + color2.B * t;
+            return new Color() { R = (byte)r, G = (byte)g, B = (byte)b };
+        }
+    }
+}
---
+++ GenerativeArt/Perlin.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+
+namespace GenerativeArt
+{
+    // Based on the article here:
+    //  https://adrianb.io/2014/08/09/perlinnoise.html
+
+    public class Perlin
+    {
+        private readonly int[] _p;
+        public static int Repeat => 256;
+        public int Octaves { get; init; } = 3;
+        public double Persistence { get; init; } = 1;
+        public double Frequency { get; init; } = 1;
+
+        public Perlin(int seed = -1)
+        {
+            _p = Permutations(seed);
+        }
+
+        private static int[] Permutations(int seed = -1)
+        {
+            var rnd = seed < 0 ? new Random() : new Random(seed);
+
+            var perm256 = Enumerable.Range(0, 256).ToArray();
+            for (var i = 0; i < 255; i++)
+            {
+                var iSwap = rnd.NextInt64(i, 255);
+                (perm256[i], perm256[iSwap]) = (perm256[iSwap], perm256[i]);
+            }
+            return Enumerable.Range(0, 512).Select(i => perm256[i & 255]).ToArray();
+        }
+
+        public double Value(double x, double y, double z = 0)
+        {
+            var total = 0.0;
+            var frequency = Frequency;
+            var amplitude = 1.0;
+            var maxValue = 0.0;
+
+            for (var i = 0; i < Octaves; i++)
+            {
+                total += BaseValue(x * frequency, y * frequency, z * frequency) * amplitude;
+                maxValue += amplitude;
+                amplitude *= Persistence;
+                frequency *= 2;
+            }
+            return total / maxValue;
+        }
+
+        private double BaseValue(double x, double y, double z = 0)
+        {
+            if (Repeat > 0)
+            {                                    // If we have any repeat on, change the coordinates to their "local" repetitions
+                x = x % Repeat;
+                y = y % Repeat;
+                z = z % Repeat;
+            }
+
+            var xi = (int)x & 255;                              // Calculate the "unit cube" that the point asked will be located in
+            var yi = (int)y & 255;                              // The left bound is ( |_x_|,|_y_|,|_z_| ) and the right bound is that
+            var zi = (int)z & 255;                              // plus 1.  Next we calculate the location (from 0.0 to 1.0) in that cube.
+            var xf = x - (int)x;
+            var yf = y - (int)y;
+            var zf = z - (int)z;
+
+            var u = Fade(xf);
+            var v = Fade(yf);
+            var w = Fade(zf);
+
+            var aaa = _p[_p[_p[xi] + yi] + zi];
+            var aba = _p[_p[_p[xi] + Inc(yi)] + zi];
+            var aab = _p[_p[_p[xi] + yi] + Inc(zi)];
+            var abb = _p[_p[_p[xi] + Inc(yi)] + Inc(zi)];
+            var baa = _p[_p[_p[Inc(xi)] + yi] + zi];
+            var bba = _p[_p[_p[Inc(xi)] + Inc(yi)] + zi];
+            var bab = _p[_p[_p[Inc(xi)] + yi] + Inc(zi)];
+            var bbb = _p[_p[_p[Inc(xi)] + Inc(yi)] + Inc(zi)];
+
+            var x1 = Lerp(Grad(aaa, xf, yf, zf), Grad(baa, xf - 1, yf, zf), u);
+            var x2 = Lerp(Grad(aba, xf, yf - 1, zf), Grad(bba, xf - 1, yf - 1, zf), u);
+            var y1 = Lerp(x1, x2, v);
+
+            x1 = Lerp(Grad(aab, xf, yf, zf - 1), Grad(bab, xf - 1, yf, zf - 1), u);
+            x2 = Lerp(Grad(abb, xf, yf - 1, zf - 1), Grad(bbb, xf - 1, yf - 1, zf - 1), u);
+            var y2 = Lerp(x1, x2, v);
+
+            // Transform from [-1, 1] range to [0, 1] range
+            return (Lerp(y1, y2, w) + 1) / 2;
+        }
+
+        private static double Grad(int hash, double x, double y, double z)
+        {
+            switch (hash & 0xF)
+            {
+                case 0x0: return x + y;
+                case 0x1: return -x + y;
+                case 0x2: return x - y;
+                case 0x3: return -x - y;
+                case 0x4: return x + z;
+                case 0x5: return -x + z;
+                case 0x6: return x - z;
+                case 0x7: return -x - z;
+                case 0x8: return y + z;
+                case 0x9: return -y + z;
+                case 0xA: return y - z;
+                case 0xB: return -y - z;
+                case 0xC: return y + x;
+                case 0xD: return -y + z;
+                case 0xE: return y - x;
+                case 0xF: return -y - z;
+                default: return 0; // never happens
+            }
+        }
+
+        private static double Fade(double t)
+        {
+            return t * t * t * (t * (t * 6 - 15) + 10);
+        }
+
+        private int Inc(int num)
+        {
+            return (num + 1) % Repeat;
+        }
+
+        private static double Lerp(double a, double b, double t)
+        {
+            return a + t * (b - a);
+        }
+    }
+}
